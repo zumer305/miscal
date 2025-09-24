d1cf2bbec4107f84ed5d3824f098721cd7161461
zumer305 <zumerniaz305@gmail.com>
2025-09-25 04:17:26 +0700

    push

--- backend/index.js
+++ backend/index.js
@@ -23,3 +23,86 @@ app.post("/register",(req,res)=>{
         let{user,password}=req.body; //body ka through ata postreq ma
  res.send(`Post request accepted ${user}`);
 })
+
+
+// FACTORY FUNCTION  fails bcz copies ban re hr bar
+// function personf(name,age){
+
+//     const person={
+//         name:name,
+//         age:age,
+//         talk(){
+//             console.log("talking");
+//         }
+
+//     }
+//     return person;
+// }
+// let p1=personf("zumer",18);
+// ------------------------------------------------------------------------------------
+
+
+
+// new operator 
+// function Personf(name,age){
+// this.name:name,
+// this.age:age,
+// }
+// let p2=Personf("zumer",18);
+
+// Personf.prototype.talk=function(){
+//     console.log("Hiiiii");
+// }
+// let p=new Personf("zumer",12); //new keyword
+
+// ------------------------------------------------------------------------------------
+// Class 
+// class Person{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+//     talk(){
+//         console.log("Hi  am class");
+//     }
+
+// }
+// let a= new Person("z",1);
+// let b= new Person("n",2);
+// ------------------------------------------------------------------------------------
+// CLASS INHERITANCE
+// class Person{
+//     constructor(name,age,marks){
+//         this.name=name;
+//         this.age=age;
+//         this.marks=marks;
+//     }
+//     talk(){
+//         console.log("Hi  am class");
+//     }
+
+// }
+
+
+
+// class Student extends Person{
+//     constructor(name,age,marks){
+//     super(name,age);
+//         this.marks=marks;
+//     }
+//    
+
+// }
+
+
+// class Teacher extends Person {
+//     constructor(name,age,marks){
+//        //     super(name,age);
+//         this.marks=marks;
+//     }
+//    
+// }
+
+
+
+
